0b126a56bb599636df2cb19d5c4e6bba91d2080e
Fergal Dearle <fergal.dearle@6point6.co.uk>
2018-07-13 10:11:31 +0100

    Refactored SOL scripts

--- src/scripts/create-stack.sh
+++ src/scripts/create-stack.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 function commonHelpMessage() {
-  echo "Usage: $0 [-f stack-file][-h]" >&2
-  echo "  stack-file : The CloudFormation template file" >&2
+  echo "Usage: $0 [-s stack-name][-c component][-e envrionment][-d depends][-v vpc stack name][-h]" >&2  echo "  stack-file : The CloudFormation template file" >&2
+  echo "  stack-name : The CloudFormation stack name" >&2
+  echo "  component :Optional component/service name" >&2
+  echo "  environment : The environment to create (or core)" >&2
+  echo "  depends : Optional Stack we depend on (fully qualified name)" >&2
+  echo "  vpc stack name : The VPC stack name" >&2
 }
 
 
@@ -13,17 +17,26 @@ function parseCommonArguments() {
       exit 0
   fi
 
-  while getopts ":f:s:h" opt; do
+  while getopts ":s:c:e:d:v:h" opt; do
     case "$opt" in
       h)
         commonHelpMessage
         exit 0
         ;;
-      f)
-        echo "CloudFormation template $OPTARG";CF_TEMPLATE="$OPTARG"
-        ;;
       s)
-        echo "CloudFormation stack name $OPTARG";STACK_NAME="$OPTARG"
+        echo "CloudFormation stack name $OPTARG";STACK="$OPTARG"
+        ;;
+      c)
+        echo "Component/App/Service name $OPTARG";COMPONENT="$OPTARG"
+        ;;
+      d)
+        echo "CloudFormation stack name of dependent stack $OPTARG";DEPENDS="$OPTARG"
+        ;;
+      e)
+        echo "Environment name $OPTARG";ENVIRONMENT="$OPTARG"
+        ;;
+      v)
+        echo "VPC Stack name $OPTARG";VPC_STACK="$OPTARG"
         ;;
       \?)
         echo "Invalid Option -$OPTARG" >&2
@@ -42,33 +55,69 @@ function parseCommonArguments() {
     esac
   done
 
-  if [ "$CF_TEMPLATE" = "0" ]; then
-    echo "CloudFormation template parameter is missing"
-    echo
-    commonHelpMessage
-    exit 1
-  fi
-
-  if [ "$STACK_NAME" == "" ]; then
+  if [ "$STACK" = "" ]; then
     echo "CloudFormation stack name parameter is missing"
     echo
     commonHelpMessage
     exit 1
   fi
+
+  if [ "$ENVIRONMENT" = "" ]; then
+    echo "Environment parameter is missing"
+    echo
+    commonHelpMessage
+    exit 1
+  fi
+echo "Set template name"
+  CF_TEMPLATE=./infrastructure/stacks/$STACK.json
+echo "Set template name $CF_TEMPLATE"
 }
 
 parseCommonArguments "$@"
 
 template=$(cat $CF_TEMPLATE)
 
-aws cloudformation describe-stacks --stack-name $STACK_NAME >/dev/null 2>&1
+if [ "$DEPENDS" = "" ]; then
+  echo "No dependent stacks"
+else
+  aws cloudformation describe-stacks --stack-name $DEPENDS
+
+  # >/dev/null 2>&1
+
+  DEPENDS_EXISTS=$?
+
+  if [ $DEPENDS_EXISTS -eq 0 ]; then
+    echo "$DEPENDS Stack is ready."
+  else
+    echo "$DEPENDS Stack is not ready. Stopping! Create the $DEPENDS stack first"
+    exit 1
+  fi
+fi
+
+if [ "$COMPONENT" = "" ]; then
+  STACK_NAME=$ENVIRONMENT-$STACK
+else
+  STACK_NAME=$ENVIRONMENT-$COMPONENT-$STACK
+fi
+
+CF_PARAMETERS=$(realpath ./infrastructure/config/$STACK_NAME.json)
+
+echo "Params file $CF_PARAMETERS"
+
+if [ "$VPC_STACK" = "" ]; then
+  VPC_STACK=core-vpc
+fi
+
+aws cloudformation describe-stacks --stack-name $STACK_NAME
+
+# >/dev/null 2>&1
 
 EXISTS=$?
 
 if [ $EXISTS -eq 0 ]; then
   echo "Stack exists"
   echo "Updating $STACK_NAME stack from $CF_TEMPLATE template"
-  aws cloudformation update-stack --stack-name $STACK_NAME --template-body "$template" --parameters ParameterKey=VPCCIDRROOT,ParameterValue=10.1 
+  aws cloudformation update-stack --stack-name $STACK_NAME --template-body "$template" --parameters "file:///$CF_PARAMETERS" --capabilities CAPABILITY_IAM
   UPDATING=$?
   if [ $UPDATING -eq 0 ]; then
     echo "Performing stack update"
@@ -76,6 +125,7 @@ if [ $EXISTS -eq 0 ]; then
     echo "No updates needed. Stack or parameters may not have changed"
     exit 0
   fi
+
   aws cloudformation wait stack-update-complete --stack-name $STACK_NAME
 
   SUCCESS=$?
@@ -89,7 +139,7 @@ if [ $EXISTS -eq 0 ]; then
 else
   echo "Stack does not exist"
   echo "Creating $STACK_NAME stack from $CF_TEMPLATE template"
-  aws cloudformation create-stack --stack-name $STACK_NAME --template-body "$template" --parameters ParameterKey=VPCCIDRROOT,ParameterValue=10.1
+  aws cloudformation create-stack --stack-name $STACK_NAME --template-body "$template" --parameters "file:///$CF_PARAMETERS"  --capabilities CAPABILITY_IAM
 
   aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
 
@@ -101,5 +151,4 @@ else
   else
     echo "$STACK_NAME created successfully"
   fi
-
 fi
