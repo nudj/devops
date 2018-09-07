4c2c642940f632ad87fccc02f2541566c3cd23ca
Nick Collings <nick@collingo.com>
2018-09-07 09:40:26 +0100

    minor: Add sql db connection and attempt to select from people table

--- src/js/evacuate.js
+++ src/js/evacuate.js
@@ -1,6 +1,6 @@
 require('envkey')
 const { Database } = require('arangojs')
-// const knex = require('knex')
+const knex = require('knex')
 
 const AQL_URL = `${process.env.AQL_PROTOCOL}://${process.env.AQL_HOST}:${process.env.AQL_PORT}`
 // const NOSQL_URL = `${process.env.NOSQL_PROTOCOL}://${process.env.NOSQL_HOST}:${process.env.NOSQL_PORT}`
@@ -9,18 +9,18 @@ const aql = new Database({ url: AQL_URL })
 aql.useDatabase(process.env.AQL_NAME)
 aql.useBasicAuth(process.env.AQL_USER, process.env.AQL_PASS)
 
-// const sql = knex({
-//   client: 'mysql',
-//   connection: {
-//     host: process.env.SQL_HOST,
-//     port: process.env.SQL_PORT,
-//     user: process.env.SQL_USER,
-//     password: process.env.SQL_PASS,
-//     database: process.env.SQL_NAME,
-//     charset: 'utf8mb4'
-//   }
-// })
-//
+const sql = knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.SQL_HOST,
+    port: process.env.SQL_PORT,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_NAME,
+    charset: 'utf8mb4'
+  }
+})
+
 // const nosql = new Database({ url: NOSQL_URL })
 // nosql.useDatabase(process.env.NOSQL_NAME)
 // nosql.useBasicAuth(process.env.NOSQL_USER, process.env.NOSQL_PASS)
@@ -32,15 +32,17 @@ const script = async ({ aql, sql, nosql }) => {
   const collectionCursor = aql.collection('surveys')
   const collectionCursorAll = await collectionCursor.all()
   const surveys = await collectionCursorAll.all()
-
   console.log('surveys', surveys)
+
+  const people = knex.select().from('people')
+  console.log('people', people)
 }
 
 (async () => {
   let exitCode = 0
   try {
     // await script({ aql, sql, nosql })
-    await script({ aql })
+    await script({ aql, sql })
   } catch (error) {
     console.log('\n\n', error)
     exitCode = 1
